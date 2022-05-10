cd633bcbf9de9d8463908f86e97473c4c0a2093a
kyle65463 <kyle65463@gmail.com>
2022-05-10 17:43:54 +0800

    Add basic frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Take6</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			Hello world
+		</div>
+	);
+}
+
+export default Home;
