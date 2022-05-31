7698ad04ba29ddb51d1abfdaf37099646229dde1
kyle65463 <kyle65463@gmail.com>
2022-05-31 11:09:35 +0800

    Add create room and join room buttons

--- frontend/src/modules/home_page/HomePage.tsx
+++ frontend/src/modules/home_page/HomePage.tsx
@@ -1,30 +1,35 @@
 import Button from "@common/components/Button";
 import { NameContext, SocketContext } from "@utils/context";
 import Link from "next/link";
-import { useContext, useRef } from "react";
+import { useCallback, useContext, useRef } from "react";
 
 function HomePage() {
 	const { connectServer } = useContext(SocketContext);
 	const nameRef = useRef<HTMLInputElement | null>(null);
 	const { onSetName } = useContext(NameContext);
 
-	return (
-		<div className='layout'>
-			<h1 className='text-4xl'>HOME</h1>
+	const name = "";
+	const onCreateRoom = useCallback(() => {
+		connectServer(name);
+	}, []);
 
-			<div>
-				<input ref={nameRef} type='text' />
-				<Link href='/play'>
-					<Button
-						style='primary'
-						onClick={() => {
-							connectServer(nameRef.current?.value ?? "");
-							onSetName(nameRef.current?.value ?? "");
-						}}
-					>
-						Play
-					</Button>
-				</Link>
+	const onJoinRoom = useCallback(() => {
+		connectServer(name);
+	}, []);
+
+	return (
+		<div className='layout flex'>
+			<div className='flex flex-1 h-full flex-col w-full justify-center items-center'>
+				<h1 className='text-5xl'>誰是牛頭王</h1>
+				<div className='flex flex-row justify-center mt-20 mb-20'>
+					<button onClick={onCreateRoom} className='btn btn-primary w-56 h-36 text-2xl'>
+						Create room
+					</button>
+					<div className='w-20' />
+					<button onClick={onJoinRoom} className='btn btn-primary w-56 h-36 text-2xl'>
+						Join room
+					</button>
+				</div>
 			</div>
 		</div>
 	);
