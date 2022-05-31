f6368f4cc323b86afbfb95f8bda944b623d862c5
Michael <mmm323790@gmail.com>
2022-05-31 16:11:31 +0800

    clean unused game event

--- frontend/src/models/game_events.ts
+++ frontend/src/models/game_events.ts
@@ -27,29 +27,6 @@ export interface GameOverEvent extends GameEvent {
 	winners: (Player | SelfPlayer)[];
 }
 
-export interface AllPlayerPlayedEvent extends GameEvent {
-	// The list of players and the card they played for this round
-	playedCardInfo: { playerName: string; card: Card }[]; // length === numPlayer
-}
-
-export interface AppendRowEvent extends GameEvent {
-	playerName: string; // The player played the card
-	card: Card; // The played card
-	rowIdx: number; // The target row of the played card
-}
-
-export interface ClearRowEvent extends GameEvent {
-	playerName: string; // The player played the card
-	card: Card; // The new leftmost card of the row
-	rowIdx: number; // The target row to clear
-}
-
-export interface StartRowSelectionEvent extends GameEvent {
-
-}
-
-export interface StartCardSelectionEvent extends GameEvent {}
-
 export interface UpdateGameStatusEvent extends GameEvent {
 	player: SelfPlayer;
 	otherPlayers: Player[];
